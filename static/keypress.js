21ac7e1a60ef504b24b3def78cdb8399c2bdf669
Angie Roscioli <anr5151@gmail.com>
2016-08-08 20:05:27 +0000

    adding sound support and adding regex filter to limit action to letter keypresses only

--- static/keypress.js
+++ static/keypress.js
@@ -1,11 +1,20 @@
 'use strict';
 
+function isLetter(char){
+    return /^[A-Za-z]$/.test(char);
+}
+
 function onKeyPress(evt) {
     var keyPressed = evt.key;
-    var img_filepath = 'static/letters/' + keyPressed + '.png';
-    var sound_filepath = 'static/sounds/' + keyPressed + '.mp3';
 
-    $('#image').attr('src', img_filepath);
+    if (isLetter(keyPressed)){
+        var img_filepath = 'static/images/' + keyPressed + '.png';
+        var sound_filepath = 'static/sounds/' + keyPressed + '.mp3';
+        var audio = new Audio(sound_filepath);
+        audio.play();
+
+        $('#image').attr('src', img_filepath);
+    }
 }
 
 $(document).keypress(onKeyPress);
