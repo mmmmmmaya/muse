d10f35029825925c52e96bddf412f158a0fce35d
Angie Roscioli <anr5151@gmail.com>
2016-08-08 22:10:33 +0000

    first attempt of database schema

---
+++ muse.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    email varchar(50) NOT NULL UNIQUE,
+    password varchar(50) NOT NULL,
+    zipcode varchar(10),
+    country varchar(2)
+);
+
+CREATE TABLE Themes (
+    id SERIAL PRIMARY KEY,
+    name varchar(25) NOT NULL UNIQUE
+);
+
+CREATE TABLE Recordings (
+    id SERIAL PRIMARY KEY,
+    public boolean NOT NULL DEFAULT True,
+    theme_id int REFERENCES Themes(id) NOT NULL,
+    user_id int REFERENCES Users(id),
+    created_at timestamp DEFAULT now()
+);
+
+CREATE TABLE Views (
+    id SERIAL PRIMARY KEY,
+    recording_id int REFERENCES Recordings(id),
+    ip_address inet,
+    viewed_at timestamp DEFAULT now()
+);
