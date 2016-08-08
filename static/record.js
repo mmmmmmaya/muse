9b2cd1922a1c4402ce2378d805b560b83fd80e02
Angie Roscioli <anr5151@gmail.com>
2016-08-08 22:35:42 +0000

    set up the idea of recording in the app

--- static/record.js
+++ static/record.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// set up toggle-ability for app recording
+var recording = false;
+
+function toggleRecording(evt) {
+    recording = !recording;
+}
+
+$('#record-button').click(toggleRecording);
+
+
+// validate key pressed is a letter
+// and action app based on input
 function isLetter(char){
     return /^[A-Za-z]$/.test(char);
 }
@@ -7,12 +19,12 @@ function isLetter(char){
 function onKeyPress(evt) {
     var keyPressed = evt.key;
 
-    if (isLetter(keyPressed)){
+    if (recording && isLetter(keyPressed)){
         var img_filepath = 'static/images/' + keyPressed + '.png';
         var sound_filepath = 'static/sounds/' + keyPressed + '.mp3';
         var audio = new Audio(sound_filepath);
-        audio.play();
 
+        audio.play();
         $('#image').attr('src', img_filepath);
     }
 }
