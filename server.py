053a218c6ee6c1c5f072e4fe23261888de3b1ed3
Angie Roscioli <anr5151@gmail.com>
2016-08-09 00:12:09 +0000

    mock send song to server

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """Show main music-making page to user."""
+
     return render_template('index.html')
+
+
+@app.route('/api/save_song', methods=['POST'])
+def save_song():
+    """Save song data to the database."""
+
+    json = request.get_json()
+    print json
+
+    return jsonify({'response': 'OK'})
 
 
 if __name__ == '__main__':
--- static/record.js
+++ static/record.js
@@ -2,12 +2,31 @@
 
 // set up toggle-ability for app recording
 var recording = false;
+var song = [];
 
-function toggleRecording(evt) {
+function toggleRecording() {
     recording = !recording;
 }
 
-$('#record-button').click(toggleRecording);
+function sendSongToServer() {
+    var params = {
+        "song": song
+    };
+
+    $.post("/api/save_song", params, function (data) {
+        song = [];
+    });
+}
+
+function recordButtonPressed(evt){
+    if (recording) {
+        sendSongToServer();
+    }
+
+    toggleRecording();
+}
+
+$('#record-button').click(recordButtonPressed);
 
 
 // validate key pressed is a letter
@@ -16,16 +35,29 @@ function isLetter(char){
     return /^[A-Za-z]$/.test(char);
 }
 
+function actionApp(keyPressed) {
+    var imgFilepath = 'static/images/' + keyPressed + '.png';
+    var soundFilepath = 'static/sounds/' + keyPressed + '.mp3';
+
+    var audio = new Audio(soundFilepath);
+
+    audio.play();
+    $('#image').attr('src', imgFilepath);
+}
+
+function updateSong(keyPressed) {
+    song.push({
+        "timestamp": Date.now(),
+        "key": keyPressed
+    });
+}
+
 function onKeyPress(evt) {
     var keyPressed = evt.key;
 
     if (recording && isLetter(keyPressed)){
-        var imgFilepath = 'static/images/' + keyPressed + '.png';
-        var soundFilepath = 'static/sounds/' + keyPressed + '.mp3';
-        var audio = new Audio(soundFilepath);
-
-        audio.play();
-        $('#image').attr('src', imgFilepath);
+        actionApp(keyPressed);
+        updateSong(keyPressed);
     }
 }
 
