bd59cd372370305759b5a049efaf72a8bda543d6
Angie Roscioli <anr5151@gmail.com>
2016-08-11 23:58:15 +0000

    testing stub for playback utils

---
+++ tests/test_playback.py
@@ -0,0 +1,13 @@
+import unittest
+
+from model import connect_to_db, db, Recording
+
+
+class Test(unittest.TestCase):
+    """Hold off on writing these tests until I decide if I'm going to
+    change the table schema for KeyPresses."""
+
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
