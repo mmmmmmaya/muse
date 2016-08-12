c471551015f9f44f0ace5c2051a16ac301ae2abe
Angie Roscioli <anr5151@gmail.com>
2016-08-12 00:48:49 +0000

    more testing helper functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,39 @@
-from model import db, User
+import time
+
+from model import db, KeyPress, Recording, Theme, User
+
+
+def populate_test_db_keypresses():
+    """Add recording data in fake db for testing."""
+
+    for num in range(6):
+        pressed_at = int(time.time()) + num
+        fake_keypress = KeyPress(recording_id=1,
+                                 key_pressed='a',
+                                 pressed_at=pressed_at,
+                                 theme=1)
+
+        db.session.add(fake_keypress)
+
+    db.session.commit()
+
+
+def populate_test_db_recordings():
+    """Add keypress data in fake db for testing."""
+
+    fake_recording = Recording(user_id=1)
+
+    db.session.add(fake_recording)
+    db.session.commit()
+
+
+def populate_test_db_themes():
+    """Create theme data in fake db for testing."""
+
+    fake_theme = Theme(name='fake_theme')
+
+    db.session.add(fake_theme)
+    db.session.commit()
 
 
 def populate_test_db_users():
