85d2fb9f6b46b553d1dc6abda367edb504c13e9c
Angie Roscioli <anr5151@gmail.com>
2016-08-15 17:30:32 +0000

    settle on timer

--- static/js/playback.js
+++ static/js/playback.js
@@ -1,34 +1,31 @@
 'use strict';
 
 function waitThenPress(keypress, waitTime) {
-    // TODO make a more accurate timer
     setTimeout(function(){
         actionApp(keypress.next_key);
     }, waitTime);
 }
 
 function playbackRecording(content) {
-    var waitTime = 0;
-    var keypress = content[0];
-    actionApp(keypress.this_key);
+    var waitTime = 1;
 
     for (var i = 0; i < content.length; i++) {
-        waitTime += keypress.time_to_next;
         keypress = content[i];
-
         waitThenPress(keypress, waitTime);
+        waitTime += keypress.time_to_next;
     }
 }
 
-function playbackLinkClicked(evt) {
+function playbackButtonClicked(evt) {
     var urlString = '/fetch_recording/' + this.id;
 
     $.get(urlString, function(data){
         if (data.status === 'success') {
             playbackRecording(data.content);
+        } else {
+            alert('Could not load recording.');
         }
-        // TODO else{ display error or something }
     });
 }
 
-$('.playback').click(playbackLinkClicked);
+$('.playback').click(playbackButtonClicked);
