bf073dc39a3a6d43f01e29ddce18b50dca48b009
Angie Roscioli <anr5151@gmail.com>
2016-08-17 00:21:25 +0000

    actually finalizing acoustic theme colors

--- static/js/themes.js
+++ static/js/themes.js
@@ -2,15 +2,15 @@ var themeColors = [
     ['#fd9696', '#fdca96', '#fdfd96', '#cafd96', '#96cafd', '#9696fd'],  // bubbly
     ['#802e0a', '#cfb085', '#ffffff', '#afcfaf', '#85c9cf', '#6f4e37'],  // meditative
     ['#f2f3f4', '#91a3b0', '#e7feff', '#b2beb5', '#6e7f80', '#a2a2d0'],  // dubstep
-    ['#b06f84', '#b07b6f', '#b09b6f', '#a5b06f', '#6fa5b0', '#7b6fb0'],  // acoustic
+    ['#ffffff', '#fce4a5', '#101936', '#bd8173', '#40233f', '#2c1e3f'],  // acoustic
     ['#ff005a', '#ffa500', '#daff00', '#5aff00', '#005aff', '#a500ff']   // weird al
 ]
 
-var grays = [
+var backgrounds = [
     '#ededed',  // bubbly
     '#fff5ee',  // meditative
     '#1f1f1f',  // dubstep
-    '#5a5a5a',  // acoustic
+    '#eac193',  // acoustic
     '#ffffff'   // weird al
 ]
 
@@ -23,5 +23,5 @@ function getThemeGray(theme) {
 }
 
 function updateBgColor(theme) {
-    $('body').css("background-color", grays[theme]);
+    $('body').css("background-color", backgrounds[theme]);
 }
