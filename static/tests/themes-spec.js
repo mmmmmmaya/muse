ed8bdba0704cf008a6ce4cd2b0d273dd373430d2
Angie Roscioli <anr5151@gmail.com>
2016-08-24 21:43:21 +0000

    js tests for themes.js

---
+++ static/tests/themes-spec.js
@@ -0,0 +1,33 @@
+describe('Tests theme getters and setters functionality', function() {
+
+    it('should know how many themes there are', function () {
+        var numThemes = getNumThemes();
+
+        expect(numThemes).toBe(5);
+    });
+
+    it ('should know theme color sets', function() {
+        var themeColors = getThemeColors(1);
+
+        expect(themeColors.length).toBe(6);
+        expect(themeColors[0]).toBe('#fd9696');
+    });
+
+    it('should know the background color for a given theme', function() {
+        var bgColor = getThemeBg(1);
+
+        expect(bgColor).toBe('#ededed');
+    });
+
+    it('should update the bgColor to match that of a given theme', function() {
+        updateBgColor(2);
+
+        // bg color is stored as rgb in css, but my colors are stored as
+        // hex codes. I'll just hard code this color value as rgb instead
+        // of trying to calculate it.
+        var themeBgColor = 'rgb(255, 245, 238)';
+        var currentBgColor = $('body').css('background-color');
+
+        expect(currentBgColor).toBe(themeBgColor);
+    })
+});
