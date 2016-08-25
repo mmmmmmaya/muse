1bb8d582be06c0771befc451730a6e8a4bb711be
Angie Roscioli <anr5151@gmail.com>
2016-08-25 22:57:19 +0000

    share recordings to facebook

--- static/js/profile.js
+++ static/js/profile.js
@@ -59,3 +59,21 @@ function toggleRecordingPublic(evt) {
 }
 
 $('.toggle-public').click(toggleRecordingPublic);
+
+
+function setupFacebookButtons(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+
+    if (d.getElementById(id)) {
+        return;
+    }
+
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+    fjs.parentNode.insertBefore(js, fjs);
+}
+
+$(document).ready(function() {
+        setupFacebookButtons(document, 'script', 'facebook-jssdk');
+});
