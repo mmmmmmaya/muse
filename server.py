a818f5e4e51e7aa56ca3492ef9cde3eef1ee9fb8
Angie Roscioli <anr5151@gmail.com>
2016-09-02 22:24:21 +0000

    any port

--- server.py
+++ server.py
@@ -356,4 +356,5 @@ if __name__ == '__main__':
 
     app.debug = True
     connect_to_db(app)
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
