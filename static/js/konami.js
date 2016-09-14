8715b6cb571fb605223bc9a4d3eb4dd4d1d6efaf
Angie Roscioli <anr5151@gmail.com>
2016-09-14 00:49:34 +0000

    finishing konami

--- static/js/konami.js
+++ static/js/konami.js
@@ -1,9 +1,11 @@
 var codeProgress = [];
+var dirs = ['left', 'down', 'up', 'right'];
 var konamiCode = ['arrowup', 'arrowup',
                   'arrowdown', 'arrowdown',
                   'arrowleft', 'arrowright',
                   'arrowleft', 'arrowright',
                   'b', 'a'];
+var score = 0;
 
 /** Functions to Track User Progress **/
 function konamiTracker(evt) {
@@ -56,8 +58,8 @@ function konamiRedirect() {
 function playKonamiPrize() {
     $('#home-button').addClass('hide');
 
-    playSong();
     playDDR();
+    playSong();
 
     $('#home-button').removeClass('hide');
 }
@@ -73,14 +75,15 @@ function playDDR() {
     var gap = size / 8;
 
     showTopArrows(size, gap);
-    updateScore(0);
+    updateScore();
 
-    scrollArrows(size, gap);
+    $(document).keydown(keepScore);
+
+    getRecordedArrows(size, gap);
     flashScore();
 }
 
 function getXDims(size, gap) {
-    var dirs = ['left', 'down', 'up', 'right'];
     var startX = (svgWidth / 2) - ((2 * size) + (1.5 * gap));
     var xDims = {
 
@@ -96,7 +99,6 @@ function getXDims(size, gap) {
 }
 
 function showTopArrows(size, gap) {
-    var dirs = ['left', 'down', 'up', 'right'];
     var xDims = getXDims(size, gap);
 
     for (var i = 0; i < dirs.length; i++) {
@@ -114,27 +116,24 @@ function showTopArrows(size, gap) {
     }
 }
 
-function getRecordedArrows() {
-  return[{'time_to_next_arrow': 500,
-          'direction': 'up'},
-          {'time_to_next_arrow': 500,
-          'direction': 'down'},
-          {'time_to_next_arrow': 500,
-          'direction': 'left'},
-          {'time_to_next_arrow': 0,
-          'direction': 'right'}]
+function getRecordedArrows(size, gap) {
+    $.get('/fetch_konami', function(data) {
+        var arrows = data.content;
+        scrollArrows(size, gap, arrows);
+    });
 }
 
 function makeArrow(direction, size, x) {
     var imagePath = '/static/images/color_' + direction + '.png';
+    var className = 'arrow-scroll ' + direction;
 
     topSVGLayer.append('image')
                .attr('x', x)
-               .attr('y', svgHeight)
+               .attr('y', size * 10)
                .attr('height', size)
                .attr('width', size)
                .attr('xlink:href', imagePath)
-               .attr('class', 'arrow-scroll');
+               .attr('class', className);
 }
 
 function makeAndScrollArrow(direction, size, x, waitTime) {
@@ -143,8 +142,7 @@ function makeAndScrollArrow(direction, size, x, waitTime) {
     }, waitTime);
 }
 
-function scrollArrows(size, gap) {
-    var arrows = getRecordedArrows();
+function scrollArrows(size, gap, arrows) {
     var waitTime = 1;
     var xDims = getXDims(size, gap);
 
@@ -159,11 +157,51 @@ function scrollArrows(size, gap) {
     }
 }
 
-function updateScore(score) {
+
+/** Functions to keep score **/
+function updateScore() {
     var html = 'Score: ' + score;
     $('#score').html(html);
 }
 
 function flashScore() {
     $('#score').addClass('ddr-score');
+}
+
+function isArrow(key) {
+    var isArrow = false;
+
+    if (dirs.indexOf(key) > -1) {
+        isArrow = true;
+    }
+
+    return isArrow;
+}
+
+function checkProximityAndUpdateScore(keyPressed) {
+    var selector = '#konami-svg g .arrow-scroll.' + keyPressed;
+    var arrows = $(selector);
+
+    var topArrows = $('.top-arrow');
+    var topArrowOffset = topArrows.offset().top;
+
+    for (var i = 0; i < arrows.length; i++) {
+        var arrow = $(arrows[i]);
+        var arrowOffset = arrow.offset().top;
+
+        if (Math.abs(arrowOffset - topArrowOffset) < 30) {
+            arrow.hide();
+            score += 10;
+        }
+    }
+
+    updateScore();
+}
+
+function keepScore(evt) {
+    var keyPressed = evt.key.toLowerCase().replace('arrow', '');
+
+    if (isArrow(keyPressed)) {
+        checkProximityAndUpdateScore(keyPressed);
+    }
 }
